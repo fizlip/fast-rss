2e1a08c2dc032470ca14557b1116d50d4766acd6
fizlip <fillatino@gmail.com>
2024-09-06 14:49:11 +0200

    read items from array of urls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::BufReader;
+use rss::Channel;
+use rss::Item;
+
+async fn read_rss(url: &str) -> Option<Vec<Item>>{
+  let content = reqwest::get(url)
+        .await.ok()?
+        .bytes()
+        .await.ok()?;
+
+  let channel = Channel::read_from(&content[..]).ok()?;
+  Some(channel.items)
+}
+
+fn print_items(items: Vec<Item>) {
+  for item in items {
+    println!("{:?}", item.title.unwrap());
+    println!("{:?}", item.link.unwrap());
+    println!("\n")
+  }
+}
+
+async fn consume_feed(url:&str) -> Result<(), &str>{
+  let items = read_rss(url).await.ok_or("Could not fetch rss feed")?;
+  print_items(items);
+  Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let feeds = vec![
+    "https://www.reutersagency.com/feed/?best-topics=political-general&post_type=best",
+    "https://techcrunch.com/feed",
+  ]; 
+  
+  for f in feeds {
+    consume_feed(f).await;
+  }
+  Ok(())
 }
